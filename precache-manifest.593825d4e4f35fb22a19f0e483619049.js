4230c50880152d8b34f9f19a502ede7c8b56f02c
Eduardo Andrade <edu.p.andrade@gmail.com>
2021-04-06 14:53:33 -0500

    Updates

---
+++ precache-manifest.593825d4e4f35fb22a19f0e483619049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "066c9cdadc52570f2dc8e051c04584a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d977066a9b41c5951df6",
+    "url": "/static/css/2.4c51956b.chunk.css"
+  },
+  {
+    "revision": "4c778eea3e4b974659ca",
+    "url": "/static/css/main.3b5d2baf.chunk.css"
+  },
+  {
+    "revision": "d977066a9b41c5951df6",
+    "url": "/static/js/2.cd7dd0a0.chunk.js"
+  },
+  {
+    "revision": "4c778eea3e4b974659ca",
+    "url": "/static/js/main.4fdaf8a2.chunk.js"
+  },
+  {
+    "revision": "44c417d58be4c7826c5c",
+    "url": "/static/js/runtime~main.39c9ef45.js"
+  }
+]);
